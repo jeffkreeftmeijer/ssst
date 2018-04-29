772e48c9ae8b3a4850cdc9d0aec02a326972510e
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-04-29 12:18:53 +0200

    Remove deps from mix project

--- mix.exs
+++ mix.exs
@@ -6,8 +6,7 @@ defmodule Ssst.MixProject do
       app: :ssst,
       version: "0.1.0",
       elixir: "~> 1.7-dev",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      start_permanent: Mix.env() == :prod
     ]
   end
 
@@ -15,14 +14,6 @@ defmodule Ssst.MixProject do
   def application do
     [
       extra_applications: [:logger, :inets, :ssl]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
