31abc2d20b081aa7d2ad47063d520ab26384fb79
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-04-29 11:10:18 +0200

    $ mix new ssst

---
+++ lib/ssst.ex
@@ -0,0 +1,18 @@
+defmodule Ssst do
+  @moduledoc """
+  Documentation for Ssst.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ssst.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ssst_test.exs
@@ -0,0 +1,8 @@
+defmodule SsstTest do
+  use ExUnit.Case
+  doctest Ssst
+
+  test "greets the world" do
+    assert Ssst.hello() == :world
+  end
+end
